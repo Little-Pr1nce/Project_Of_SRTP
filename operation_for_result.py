d7e0ecfcafc7ea784d41096736778d738633e196
Michael-sn <503033454@qq.com>
2018-05-27 20:50:48 +0800

    improve
    完善调用api后的处理代码，新增一个单词库处理模块

--- operation_for_result.py
+++ operation_for_result.py
@@ -5,8 +5,14 @@ from using_baidu_webapi import get_token, recognize
 filename = "16k.wav"                                # 音频文件名称，必要的时候要写绝对地址
 rate = 16000                                        # 音频文件的码率
 form = 'wav'
-signal = open(filename, "rb").read()                # 读取文件
+signal = open(filename, "rb").read()                # 以二进制读取文件
 
 token = get_token()
 first_result = recognize(signal, rate, token, form)
-print(first_result)
+print(first_result)                                 # 打印的到的语音结果
+
+"""讲得到结果，保存为txt文件，以便后续的处理"""
+txt_filname = 'result_from_webapi.txt'
+with open(txt_filname, 'a') as file_object:
+    txt = '未经纠错的语音识别文本：' + first_result + '\n'
+    file_object.write(txt)
---
+++ operation_for_vocabularylib.py
@@ -0,0 +1,72 @@
+from collections import OrderedDict
+from pyexcel_xls import save_data
+from pyexcel_xls import get_data
+import pypinyin
+
+"""处理单词库，添加拼音进入单词库"""
+"""查看资料网址：https://www.cnblogs.com/alfred0311/p/7809863.html"""
+
+"""pyexcel_xls 以 OrderedDict 结构处理数据"""
+
+
+def read_xls_file_print(fil):
+    """读取xls文件,并打印出结果  """
+    """fil为读取文件的绝对路径"""
+    data = get_data(fil)
+    print("数据格式：",type(data))
+    for sheet_n in data.keys():
+        print(sheet_n, ":", data[sheet_n])
+
+
+def read_xls_file_return(fil):
+    """读取xls文件,并返回出结果"""
+    """fil为读取文件的绝对路径"""
+    data = get_data(fil)
+    for sheet_n in data.keys():
+        return data[sheet_n]
+
+
+def str_of_result(res):
+    """把得到的列表文件中的元素从列表转化到字符串，并处理字符串可以直接被pinyin模块识别"""
+    """参数res就是读取xls文件得到的那个列表，是read_xls_file两个函数的返回结果"""
+    row = []
+    for value in range(0, len(res)):
+        no1     = res[value]
+        no11    = str(no1)
+        last_no = no11[2:len(no11) - 2]
+        row.append(last_no)
+    return row
+
+
+def addition_pinyin(data, num):
+    """得到一个有症状名和对应拼音的列表,返回这个列表"""
+    """data参数是str_of_result函数处理后的列表，num是处理后列表的长度"""
+    row = []  # 新建一个空列表
+    for value in range(0, num):
+        py  = pypinyin.slug(data[value])  # 每个词的拼音
+        tem = [data[value], py]
+        row.append(tem)
+    return row
+
+
+def save_xls_file(res, fil):
+    """把处理后的列表保存在xls格式文件里面"""
+    """res就是addition_pinyin函数处理后的列表，fil就是xls文件的绝对地址"""
+    data = OrderedDict()
+    # sheet表数据
+    sheet_1    = []
+    row_1_data = ['症状名', '拼音']  # 第一行的数据
+    sheet_1.append(row_1_data)
+    # 逐行添加症状名和数据
+    for value in range(0,len(res)):
+        initial = res[value]
+        symptom = initial[0]
+        py      = initial[1]
+        row_value_data = [symptom, py]
+        sheet_1.append(row_value_data)
+    # 添加sheet表
+    data.update({"sheet1": sheet_1})
+    # 保存为xls文件
+    save_data(fil, data)
+
+
